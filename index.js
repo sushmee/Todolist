f874669b919417551683113928b4b25472a5215d
sushmee <86013254+sushmee@users.noreply.github.com>
2022-03-20 22:27:27 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+import ReactDOM from "react-dom";
+
+import "./index.css";
+ import ToDoList from "./ToDoList";
+ ReactDOM.render(<ToDoList />, document.getElementById("root"));
